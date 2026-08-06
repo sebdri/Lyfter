86ab6be40001f3daa3b10fdec1005d8889578795
Sebdri <sebdri@outlook.com>
2026-08-05 23:11:56 -0600

    LOS 4 PILARES DE LA POO

--- Python Intermedio/4PilarerPOO/Ejercicios_4Pilaries.py
+++ Python Intermedio/4PilarerPOO/Ejercicios_4Pilaries.py
@@ -170,4 +170,130 @@ sexual_animal_b = SexualAnimal()
 sexual_animal.reproduce(sexual_animal_b) # -> Reproducing in a sexual manner with sexual_animal_b
 
 # animal = Animal() # va a fallar porque Animal es una clase abstracta
-# other_animal = OtherAnimal() # va a fallar porque no se sobre-escribió el método reproduce
+
+
+
+
+
+# other_animal = OtherAnimal() # va a fallar porque no se sobre-escribió el método reproduce#                                   ==={Clases abstractas}===
+"""
+public: pueden ser accesados desde cualquier lugar.
+protected: pueden ser accesados desde los métodos de la clase y sus clases hijas.
+private: pueden ser accesados solo desde los métodos de la clase.
+
+"""
+class BankAccount():
+	balance = 0
+
+	def __substract_balance(self, amount):
+		self.balance -= amount
+
+	def __add_balance(self, amount):
+		self.balance += amount
+
+	def send_money_to_account(self, account, amount):
+		self.__substract_balance(amount)
+		account.__add_balance(amount)
+
+
+bank_account = BankAccount()
+bank_account.__add_balance(5000)
+
+class Person:
+	name: string #public
+	_date_of_birth: datetime #protected
+	__sex: string # private
+
+	def __init__(self, name, date_of_birth, sex)
+		self.name = name
+		self._date_of_birth = date_of_birth
+		self.__sex = sex
+
+class Worker(Person):
+	def print_date_of_birth(self):
+		print(self._date_of_birth)
+
+	def print_sex(self):
+		print(self.__sex)
+
+my_person = Person("Juan", "2003/02/02", "Male")
+print(my_person.name) # -> Juan
+print(my_person._date_of_birth) # -> error, ya que _date_of_birth es un atributo protegido
+print(my_person.__sex) # -> error, ya que __sex es un atributo privado
+
+my_worker = Worker("Joan", "1984/05/06", "Female")
+my_worker.print_date_of_birth() # -> 1984/05/06
+my_worker.print_sex() # -> error, ya que __sex es un atributo privado
+
+
+
+
+
+#                                   ==={Abstracción}===
+'''
+La abstracción permite poder trabajar con objetos de manera efectiva sin necesidad de 
+conocer todos los detalles internos de los mismos.
+
+Por ejemplo, cuando manejamos un carro, no necesitamos saber cómo funciona su 
+motor a nivel interno.
+
+'''
+
+class Person:
+	name: string
+	_date_of_birth: datetime
+
+	def __init__(self, name, date_of_birth)
+		self.name = name
+		self._date_of_birth = date_of_birth
+
+	def get_age(self)
+		return today() - self._date_of_birth
+
+
+
+
+
+
+	#                                   ==={Abstracción}===
+
+
+'''
+Se refiere a que una sintaxis o método con el mismo identificador pueden 
+tener comportamientos o formas distintas dependiendo del contexto en que se usen.
+
+'''
+
+class Vehiculo:
+	is_on: bool
+	ruedas: int
+
+	def encender(self):
+		self.is_on = True
+		print("Vehiculo encendida")
+
+	def apagar(self):
+		self.is_on = False
+
+	class Computadora:
+	is_on: bool
+
+	def encender(self):
+		self.operative_system = "Windows"
+		self.is_on = True
+		print("Computadora encendida")
+
+	def apagar(self):
+		self.is_on = False
+
+
+
+object_list = [
+	Vehiculo(),
+	Vehiculo(),
+	Computadora(),
+	Computadora(),
+]
+
+for object in object_list:
+	object.encender()
